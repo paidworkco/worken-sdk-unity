cd283c4248f0c645078e40891876c7e650dd4a0a
Axlii50 <kubad4d@wp.pl>
2024-02-20 16:07:48 +0100

    Tworzenie portfela oraz pozyskiwanie kolejnego Nonce na podstawie Konta netherowego

--- worken-sdk-unity/Wallet/WalletMenager.cs
+++ worken-sdk-unity/Wallet/WalletMenager.cs
@@ -1,26 +1,42 @@
-﻿using System;
+﻿using NBitcoin;
+using Nethereum.Hex.HexTypes;
+using Nethereum.Web3;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wallet = Nethereum.HdWallet.Wallet;
 
-namespace worken_sdk_unity.Wallet
+namespace worken_sdk_unity.WalletMenager
 {
-    public sealed class WalletMenager
+    public sealed class WalletManager
     {
-        public async Task CreateWallet()
+        public async Task<Wallet> CreateWallet()
         {
-            throw new NotImplementedException("CreateWallet method is not implemented.");
-        }
+            Mnemonic mnemo = new Mnemonic(Wordlist.English, WordCount.Twelve);
 
-        public async Task GetWalletInformation()
-        {
-            throw new NotImplementedException("GetWalletInformation method is not implemented.");
+            var wallet = new Nethereum.HdWallet.Wallet(mnemo.WordList, WordCount.Twelve);
+            
+            return wallet;
         }
 
         public async Task GetWalletHistory()
         {
             throw new NotImplementedException("GetWalletHistory method is not implemented.");
+        }
+    }
+
+    public static class WalletMenagerExtensions
+    {
+        /// <summary>
+        /// zwraca Nonce
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        public async static Task<HexBigInteger> GetNextAccountNonce(this Nethereum.Web3.Accounts.Account account)
+        {
+            return await account.NonceService.GetNextNonceAsync();
         }
     }
 }
