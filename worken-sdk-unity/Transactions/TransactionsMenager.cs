879ccce33c19eb5353fe317439762f2eb63569ae
Axlii50 <kubad4d@wp.pl>
2024-02-20 16:06:29 +0100

    Przygotowana funkcja do wysyłania transakcji wraz z stworzonymi modelami na kiedyś według dokumentacji

--- worken-sdk-unity/Transactions/TransactionsMenager.cs
+++ worken-sdk-unity/Transactions/TransactionsMenager.cs
@@ -1,34 +1,68 @@
-﻿using System;
+﻿using Nethereum.Hex.HexTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using worken_sdk_unity.Network;
+using Account = Nethereum.Web3.Accounts.Account;
+using Nethereum.Web3;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts.CQS;
+using Nethereum.Util;
+using Nethereum.Web3.Accounts;
+using Nethereum.Hex.HexConvertors.Extensions;
+using Nethereum.Contracts;
+using Nethereum.Contracts.Extensions;
+using System.Numerics;
+using worken_sdk_unity.Transactions.model;
+using Nethereum.RPC.Eth.DTOs;
 
 namespace worken_sdk_unity.Transactions
 {
-    public sealed class TransactionsMenager
+    public static class TransactionsManager
     {
-        public async Task SendTransaction()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="account"></param>
+        /// <param name="To"></param>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public async static Task<string> SendTransaction(this Account account, string To, HexBigInteger amount)
         {
-            throw new NotImplementedException("SendTransaction method is not implemented.");
+            var Web3Client = NetworkManager.GetAccountWeb3Full(account);
+
+            var transferHandler = Web3Client.Eth.GetContractTransactionHandler<TransferFunction>();
+
+            var transfer = new TransferFunction()
+            {
+                To = To,
+                TokenAmount = amount.Value
+            };
+
+            var receipt = await transferHandler.SendRequestAndWaitForReceiptAsync(WorkenSDKUnity.ContractAddress, transfer);
+
+            Web3Client = null;
+            return receipt.TransactionHash;
         }
 
-        public async Task ReceiveTransaction()
+        public async static Task ReceiveTransaction()
         {
             throw new NotImplementedException("ReceiveTransaction method is not implemented.");
         }
 
-        public async Task GetTransactionStatus()
+        public async static Task GetTransactionStatus()
         {
-            throw new NotImplementedException("GetTransactionStatus method is not implemented.");
+            
         }
 
-        public async Task GetRecentTransactions()
+        public async static Task GetRecentTransactions()
         {
             throw new NotImplementedException("GetRecentTransactions method is not implemented.");
         }
 
-        public async Task GetEstimatedGas()
+        public async static Task GetEstimatedGas()
         {
            
         }
---
+++ worken-sdk-unity/Transactions/model/BalanceOfFunction.cs
@@ -0,0 +1,17 @@
+﻿using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace worken_sdk_unity.Transactions.model
+{
+    [Function("balanceOf", "uint256")]
+    public class BalanceOfFunction : FunctionMessage
+    {
+        [Parameter("address", "_owner", 1)]
+        public string Owner { get; set; }
+    }
+}
