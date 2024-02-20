ccf729d885aa1a64bedca09f4bf9c64cc176615b
Axlii50 <kubad4d@wp.pl>
2024-02-20 12:25:38 +0100

    Dodaj pliki projektów.

---
+++ worken-sdk-unity/WorkenSDKUnity.cs
@@ -0,0 +1,30 @@
+﻿using Nethereum.RPC.Web3;
+using Nethereum.Web3;
+using System;
+
+namespace worken_sdk_unity
+{
+    internal static class WorkenSDKUnity
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public readonly static string Abi = @"[
+            {
+              ""constant"": true,
+              ""inputs"": [{""name"": ""_owner"", ""type"": ""address""}],
+              ""name"": ""balanceOf"",
+              ""outputs"": [{""name"": ""balance"", ""type"": ""uint256""}],
+              ""type"": ""function""
+            }]";
+
+        /// <summary>
+        /// Worken contract address
+        /// </summary>
+        public readonly static string ContractAddress = "0x3AE0726b5155fCa70dd79C0839B07508Ce7F0F13";
+
+        public readonly static string Url = "https://rpc-mumbai.maticvigil.com/";
+
+        public static readonly Web3 Web3Client = new(Url);
+    }
+}
