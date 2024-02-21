38313849af3ed97482faa474db86f5747a877a5c
Axlii50 <kubad4d@wp.pl>
2024-02-21 21:24:13 +0100

    GetContractStatus

---
+++ worken-sdk-unity/Contract/Contract.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace worken_sdk_unity.Contract
+{
+    public class ContractManager
+    {
+        /// <summary>
+        /// Returnes true if contract is active
+        /// </summary>
+        /// <returns></returns>
+        public async Task<bool> GetContractStatus()
+        {
+            var response = await WorkenSDKUnity.Web3Client.Eth.GetCode.SendRequestAsync(WorkenSDKUnity.ContractAddress.Replace("3AE0","3333"));
+
+            return response == "0x" ? false : true;
+        }
+    }
+}
